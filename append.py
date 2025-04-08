25a47eead3a59b9672f63b531de6194ec7ba873d
tachau03 <taminhchau8903@gmail.com>
2025-04-08 13:46:52 -0400

    append changes

--- append.py
+++ append.py
@@ -1,16 +1,35 @@
-
-def appends(n):
+import numpy as np
+import pandas as pd
+import util 
+def slow_appends(arr):
     l = []
-    for i in range(n):
-        l.append(i ** 2)
+    for x in arr:
+        l.append(x ** 101)
+    return l
 
-def list_comprehension(n):
-    l = [i ** 2 for i in range(n)]
+def faster_list_compre(arr):
+    return [i ** 101 for i in arr]
+
+def fast_numpy(arr):
+    return np.array(arr) ** 101
+
+def make_data1(size=1000):
+    np.random.seed(4)
+    array1 = np.random.rand(size, 1)  # np.array
+    df1 = pd.DataFrame(array1)  # pd.DataFrame
+    return df1
 
 def main():
-    n = 100000000
-    # appends(n)
-    list_comprehension(n)
+    input = make_data1()
+    arr = input[0].to_numpy()
+    
+    funcs = [slow_appends, faster_list_compre]
+    args = [[arr], [arr]]
+    names = ["slow_appends", "faster_list_compre"]
+    reps = 10
+    timings, data = util.time_funcs(funcs, args, names, reps)
+    util.print_time_results(timings, len(arr))
+
 
 if __name__ == "__main__":
     main()
